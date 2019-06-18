c9136a77b23159234c96bd635ca21e9fe7694f5f
Nathan Lu <nlu@ip-10-11-9-29.us-west-2.compute.internal>
2019-06-18 01:54:02 +0000

    new stuff added

---
+++ r_colorout_mod.R
@@ -0,0 +1,18 @@
+setOutputColors(
+  normal   = 22
+, negnum   = 203
+, zero     = 214
+, number   = 203
+, date     = 179
+, string   = 65
+, const    = 35
+, false    = 203
+, true     = 78
+, infinite = 39
+, index    = 30
+, stderror = 213
+, warn     = c(1, 16, 196)
+, error    = c(160, 231)
+)
+
+
